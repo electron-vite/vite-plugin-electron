e118fbd1de8a63d080a25f6e5c42ffd8b857f3f3
草鞋没号 <308487730@qq.com>
2022-06-26 15:54:28 +0800

    remove `renderer`

--- renderer/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Plugin } from 'vite';
-
-declare const electronRenderer: VitePluginElectronRenderer;
-export default electronRenderer;
-
-export interface VitePluginElectronRenderer {
-  (): Plugin[];
-}
