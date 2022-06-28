f70e0302a38223d711a3580b97dae951330f0dfa
草鞋没号 <308487730@qq.com>
2022-06-28 10:18:45 +0800

    fix(🐞): ensure always load into `buildConfig()`

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import type { Plugin } from 'vite'
 import { bootstrap } from './serve'
 import { build } from './build'
 import renderer from 'vite-plugin-electron-renderer'
+import buildConfig from 'vite-plugin-electron-renderer/plugins/build-config'
 
 export { Configuration }
 
@@ -12,19 +13,14 @@ export function defineConfig(config: Configuration) {
 
 export default function electron(config: Configuration): Plugin[] {
   const name = 'vite-plugin-electron'
-  const opts: Partial<Plugin> = {
-    config(conf) {
-      if (!conf.build) conf.build = {}
-      if (conf.build.emptyOutDir === undefined) {
-        // prevent accidental clearing of `dist/electron/main`, `dist/electron/preload`
-        conf.build.emptyOutDir = false
-      }
-    },
-  }
 
   return [
-    // Enable use Electron, Node.js API in Renderer-process
-    ...(config.renderer ? renderer(config.renderer) : []),
+    ...(config.renderer
+      // Enable use Electron, Node.js API in Renderer-process
+      ? renderer(config.renderer)
+      // There is also `buildConfig()` in `renderer()`
+      : [buildConfig()]
+    ),
     {
       name: `${name}:serve`,
       apply: 'serve',
@@ -33,7 +29,6 @@ export default function electron(config: Configuration): Plugin[] {
           bootstrap(config, server)
         })
       },
-      ...opts,
     },
     {
       name: `${name}:build`,
@@ -41,7 +36,6 @@ export default function electron(config: Configuration): Plugin[] {
       async configResolved(viteConfig) {
         await build(config, viteConfig)
       },
-      ...opts,
     },
   ]
 }
