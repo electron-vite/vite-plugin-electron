a103aa0723c7fa4049614aa90b20c68f0fbb286d
younglei <youngleish@gmail.com>
2022-07-06 22:58:55 +0800

    electron-env.d.ts

---
+++ electron-env.d.ts
@@ -0,0 +1,12 @@
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production'
+    readonly VITE_DEV_SERVER_HOST: string
+    readonly VITE_DEV_SERVER_PORT: string
+  }
+
+  interface Process {
+    electronApp: import('child_process').ChildProcessWithoutNullStreams
+  }
+}
