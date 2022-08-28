ae24483a9afb0e0c760b4d5d9f26c93a0eef0289
草鞋没号 <308487730@qq.com>
2022-08-29 07:51:52 +0800

    refactor: use `VITE_DEV_SERVER_URL`

--- playground/usecase-in-renderer/electron/main.ts
+++ playground/usecase-in-renderer/electron/main.ts
@@ -13,8 +13,7 @@ export const ROOT_PATH = {
 let win: BrowserWindow | null = null
 // Here, you can also use other preload
 const preload = path.join(__dirname, 'preload.js')
-// 🚧 Use ['ENV_NAME'] avoid vite:define plugin
-const url = `http://${process.env['VITE_DEV_SERVER_HOST']}:${process.env['VITE_DEV_SERVER_PORT']}`
+const url = process.env.VITE_DEV_SERVER_URL as string
 const indexHtml = path.join(ROOT_PATH.dist, 'index.html')
 
 function createWindow() {
