172746d4e6b3e14a6dfb653d97235097967a15e3
Aaron Reisman <lifeiscontent@users.noreply.github.com>
2022-08-30 20:45:55 -0700

    Update build-config.ts
    simplify build config using newer TS Features

--- packages/electron-renderer/src/build-config.ts
+++ packages/electron-renderer/src/build-config.ts
@@ -6,22 +6,22 @@ export default function buildConfig(): Plugin {
     apply: 'build',
     config(config) {
       // make sure that Electron can be loaded into the local file using `loadFile` after packaging
-      if (config.base === undefined) config.base = './'
+      config.base ??= './'
 
-      if (config.build === undefined) config.build = {}
+      config.build ??= {}
 
       // TODO: init `config.build.target`
       // https://github.com/vitejs/vite/pull/8843
 
       // ensure that static resources are loaded normally
       // TODO: Automatic splicing `build.assetsDir`
-      if (config.build.assetsDir === undefined) config.build.assetsDir = ''
+      config.build.assetsDir ??= ''
 
       // https://github.com/electron-vite/electron-vite-vue/issues/107
-      if (config.build.cssCodeSplit === undefined) config.build.cssCodeSplit = false
+      config.build.cssCodeSplit ??= false
 
       // prevent accidental clearing of `dist/electron/main`, `dist/electron/preload`
-      if (config.build.emptyOutDir === undefined) config.build.emptyOutDir = false
+      config.build.emptyOutDir ??= false
     },
   }
 }
