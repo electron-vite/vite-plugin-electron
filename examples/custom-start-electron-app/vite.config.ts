a75f8d1f6bdaebcf303173f8381167c3e4390ad5
草鞋没号 <308487730@qq.com>
2022-10-08 10:05:06 +0800

    feat(examples): custom-start-electron-app upgrade config to v0.10.0

--- examples/custom-start-electron-app/vite.config.ts
+++ examples/custom-start-electron-app/vite.config.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import { defineConfig } from 'vite'
-import electron, { onstart } from 'vite-plugin-electron'
+import electron from 'vite-plugin-electron'
 
 fs.rmSync('dist', { recursive: true, force: true })
 const cmds = process.argv.slice(2)
@@ -9,15 +9,11 @@ const isdev = !cmds.length || cmds.includes('dev') || cmds.includes('serve')
 export default defineConfig({
   plugins: [
     electron({
-      main: {
-        entry: 'electron/main.ts',
-        vite: {
-          plugins: [isdev && onstart(startup => {
-            /** Start Electron App */
-            startup()
-          })],
-        },
-      },
+      entry: 'electron/main.ts',
+      onstart: isdev ? startup => {
+        /** Start Electron App */
+        startup(['.', '--no-sandbox'])
+      } : undefined,
     }),
   ],
 })
