b63ed982de08de284b8043ac20d9336aad372cce
草鞋没号 <308487730@qq.com>
2022-10-09 12:49:28 +0800

    electron-renderer: hoist cjs-shim

--- packages/electron-renderer/src/cjs-shim.ts
+++ packages/electron-renderer/src/cjs-shim.ts
@@ -10,26 +10,8 @@ export default function cjsShim(): Plugin {
       config = _config
     },
     transformIndexHtml(html) {
-      const headRE = /(<\/[\s]*?head[\s]*?>)/
-
-      // ---------------------------------------- shim-exports
-      const output = config.build.rollupOptions.output
-      if (output) {
-        const formats = ['cjs', 'commonjs']
-
-        // https://github.com/electron-vite/vite-plugin-electron/issues/6
-        // https://github.com/electron-vite/vite-plugin-electron/commit/e6decf42164bc1e3801e27984322c41b9c2724b7#r75138942
-        if (
-          Array.isArray(output)
-            // Once an `output.format` is CJS, it is considered as CommonJs
-            ? output.find(o => formats.includes(o.format as string))
-            : formats.includes(output.format as string)
-        ) {
-          // fix(🐞): exports is not defined
-          const exportsShim = `<script id="shim-exports">var exports = typeof module !== 'undefined' ? module.exports : {};</script>`
-          html = html.replace(headRE, exportsShim + '\n$1')
-        }
-      }
+      const headRE = /(<\s*?head\s*?>)/
+      // const headeRE = /(<\/[\s]*?head[\s]*?>)/
 
       // ---------------------------------------- shim-require-id
       const assetsDir = config.build.assetsDir
@@ -52,7 +34,26 @@ export default function cjsShim(): Plugin {
   };
 })();
 </script>`
-        html = html.replace(headRE, requireIdShim + '\n$1')
+        html = html.replace(headRE, '$1\n' + requireIdShim)
+      }
+
+      // ---------------------------------------- shim-exports
+      const output = config.build.rollupOptions.output
+      if (output) {
+        const formats = ['cjs', 'commonjs']
+
+        // https://github.com/electron-vite/vite-plugin-electron/issues/6
+        // https://github.com/electron-vite/vite-plugin-electron/commit/e6decf42164bc1e3801e27984322c41b9c2724b7#r75138942
+        if (
+          Array.isArray(output)
+            // Once an `output.format` is CJS, it is considered as CommonJs
+            ? output.find(o => formats.includes(o.format as string))
+            : formats.includes(output.format as string)
+        ) {
+          // fix(🐞): exports is not defined
+          const exportsShim = `<script id="shim-exports">var exports = typeof module !== 'undefined' ? module.exports : {};</script>`
+          html = html.replace(headRE, '$1\n' + exportsShim)
+        }
       }
 
       return html
