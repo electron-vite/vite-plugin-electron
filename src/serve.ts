1ea7d00c2f9831e83c86d970a296ef7708397918
Jason Nall <jsonnull@gmail.com>
2022-11-10 17:02:03 -0500

    Fix react-refresh in development.

--- src/serve.ts
+++ src/serve.ts
@@ -26,6 +26,7 @@ export async function bootstrap(config: Configuration | Configuration[], server:
     const inlineConfig = withExternalBuiltins(resolveViteConfig(config))
     await viteBuild(mergeConfig(
       {
+        mode: 'development',
         build: {
           watch: {},
         },
