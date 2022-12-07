029a4ca25ac5d213e811b8ef97b2c221c1fec073
草鞋没号 <308487730@qq.com>
2022-12-07 09:59:19 +0800

    feat(examples): load `.env` file #115

--- examples/quick-start/vite.config.ts
+++ examples/quick-start/vite.config.ts
@@ -1,11 +1,39 @@
-import { defineConfig } from 'vite'
+import {
+  type Plugin,
+  defineConfig,
+  loadEnv,
+} from 'vite'
 import electron from 'vite-plugin-electron'
+
+// Load .env
+function loadEnvPlugin(): Plugin {
+  return {
+    name: 'vite-plugin-load-env',
+    config(config, env) {
+      const root = config.root ?? process.cwd()
+      const result = loadEnv(env.mode, root)
+      // Remove the vite-plugin-electron injected env.
+      delete result.VITE_DEV_SERVER_URL
+      config.esbuild ??= {}
+      config.esbuild.define = {
+        ...config.esbuild.define,
+        ...Object.fromEntries(Object.entries(result)
+          .map(([key, val]) => [`process.env.${key}`, JSON.stringify(val)])),
+      }
+    },
+  }
+}
 
 export default defineConfig({
   plugins: [
     electron([
-      // Main-Process entry file of the Electron App.
-      { entry: 'electron/main.ts' },
+      {
+        // Main-Process entry file of the Electron App.
+        entry: 'electron/main.ts',
+        vite: {
+          plugins: [loadEnvPlugin()],
+        },
+      },
       {
         entry: 'electron/preload.ts',
         onstart(options) {
