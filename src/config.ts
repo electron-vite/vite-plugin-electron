56446e2752f0e220b283dd9273908067ca87b673
草鞋没号 <308487730@qq.com>
2022-12-21 14:15:05 +0800

    chore: cleanup

--- src/config.ts
+++ src/config.ts
@@ -94,19 +94,19 @@ export function withExternalBuiltins(config: InlineConfig) {
 }
 
 /**
- * @see https://github.com/vitejs/vite/blob/c3f6731bafeadd310efa4325cb8dcc639636fe48/packages/vite/src/node/constants.ts#L131-L141
+ * @see https://github.com/vitejs/vite/blob/v4.0.1/packages/vite/src/node/constants.ts#L137-L147
  */
 export function resolveHostname(hostname: string) {
   const loopbackHosts = new Set([
     'localhost',
     '127.0.0.1',
     '::1',
-    '0000:0000:0000:0000:0000:0000:0000:0001'
+    '0000:0000:0000:0000:0000:0000:0000:0001',
   ])
   const wildcardHosts = new Set([
     '0.0.0.0',
     '::',
-    '0000:0000:0000:0000:0000:0000:0000:0000'
+    '0000:0000:0000:0000:0000:0000:0000:0000',
   ])
 
   return loopbackHosts.has(hostname) || wildcardHosts.has(hostname) ? 'localhost' : hostname
